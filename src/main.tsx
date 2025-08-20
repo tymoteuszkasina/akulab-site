95785169e2aea670e7cb2a8fe319f13f8d9fe3de
Tymoteusz Kasina <tyka@softwaremind.com>
2025-08-20 09:29:01 +0200

    Revert "Fix GitHub Pages deployment"
    This reverts commit 585499d2cad07b477588e176ee6d4e25367cf5ce.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/akulab-site">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
