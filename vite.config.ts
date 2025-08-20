94dcd652c2782d1847b04a9388574d71ce1f6ed4
Tymoteusz Kasina <tyka@softwaremind.com>
2025-08-20 09:29:53 +0200

    Revert "Set Vite base for GitHub Pages"
    This reverts commit 9240e93d368727e24cfe83abe177679d5f81bff3.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/akulab-site/', // <-- must match your repo name
-})
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+});
